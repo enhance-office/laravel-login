6d9b94fb1da1492e2f3427b162046d9fea3d76de
enhanceoffice <re_sute@yahoo.co.jp>
2024-08-16 17:30:55 +0900

    ロジックの分離

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -9,6 +9,12 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+
+    public function __construct(User $user)
+    {
+    $this->user = $user;
+    }
+
     /**
      * @return View
      */
@@ -25,10 +31,10 @@ class AuthController extends Controller
         $credentials = $request->only('email','password');
 
         //アカウントがロックされていたら弾く
-        $user = User::where('email','=',$credentials['email'])->first();
+        $user = $this->user->getUserByEmail($credentials['email']);
 
         if(!is_null($user)){
-            if($user->locked_flg === 1){
+            if($this->user->isAccountLocked($user)){
                 return back()->withErrors([
                     'danger' => 'アカウントがロックされています。',
                  ]);           
@@ -37,26 +43,20 @@ class AuthController extends Controller
             if(Auth::attempt($credentials)){
                 $request->session()->regenerate();
                 //ログイン成功したらエラーカウントを0にする
-                if($user->error_count > 0){
-                    $user->error_count = 0;
-                    $user->save();
-                }
+                $this->user->resetErrorCount($user);
 
                 return redirect()->route('home')->with('success','ログインが成功しました。');
             }
 
         //ログイン失敗したらエラーカウントを1増やす
-        $user->error_count = $user->error_count + 1;
+        $user->error_count = $this->user->addErrorCount($user->error_count);
         //エラーカウントが6以上の場合はアカウントをロックする
-        if($user->error_count > 5){
-            $user->locked_flg = 1;
-            $user->save();
+        if($this->user->lockAccount($user)){
             return back()->withErrors([
                 'danger' => 'アカウントがロックされました。解除したい場合、運営に連絡してください。',
             ]);   
         }
         $user->save();
-
         }
 
 
